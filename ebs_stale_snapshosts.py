3842518fd33c237d9c08bf14d56f1b11dceda7c8
bishwajeet0771 <69516107+bishwajeet0771@users.noreply.github.com>
2024-03-22 18:46:34 +0530

    Update ebs_stale_snapshosts.py

--- ebs_stale_snapshosts.py
+++ ebs_stale_snapshosts.py
@@ -13,16 +13,17 @@ def lambda_handler(event, context):
     for reservation in instances_response['Reservations']:
         for instance in reservation['Instances']:
             active_instance_ids.add(instance['InstanceId'])
+
     # Iterate through each snapshot and delete if it's not attached to any volume or the volume is not attached to a running instance
     for snapshot in response['Snapshots']:
         snapshot_id = snapshot['SnapshotId']
         volume_id = snapshot.get('VolumeId')
 
         if not volume_id:
-           # Delete the snapshot if it's not attached to any volume
-           ec2.delete_snapshot(SnapshotId=snapshot_id) 
-           print(f"Deleted EBS snapshot {snapshot_id} as it was not attached to any volume.")
-        else:  
+            # Delete the snapshot if it's not attached to any volume
+            ec2.delete_snapshot(SnapshotId=snapshot_id)
+            print(f"Deleted EBS snapshot {snapshot_id} as it was not attached to any volume.")
+        else:
             # Check if the volume still exists
             try:
                 volume_response = ec2.describe_volumes(VolumeIds=[volume_id])
